8398e36d2f86c2174fe6407ab54aaf4592142a5e
nfgallimore <nfgallimore@gmail.com>
2019-04-14 20:02:30 -0400

    Add recipe info

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,41 +2,46 @@ require 'httparty'
 require 'json'
 
 class RecipesController < ApplicationController
-  def index
 
+  # GET /recipes
+  def index
+    ingredients_url = ""
+    ingredients = Ingredient.all.where({ user_id: session[:current_user].id }) 
+    for i in 0 ... ingredients.size - 1
+      ingredient = CGI.escape(ingredients[i].title.downcase)
+      ingredients_url << ingredient << "%2c"
+    end
+    ingredients_url << CGI.escape(ingredients[ingredients.size - 1].title.downcase)
+
+    number = 5
+    ranking = 1
+    ignore_pantry = false
+    url =  "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?" + 
+           "number=#{number}" + 
+           "&ranking=#{ranking}" +
+           "&ignorePantry=#{ignore_pantry}" +
+           "&ingredients=#{ingredients_url}" 
     headers = { 
       "X-RapidAPI-Host" => "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
       "X-RapidAPI-Key" => "e20da5f30amsh2c5e4accedfa8e3p16b473jsnf1abb9f13716",
       "Content-Type" => "application/x-www-form-urlencoded"
     }
 
-    ingredients = Ingredient.all.where({ user_id: session[:current_user].id })
-    
-    ingredients_url = ""
-
-    for i in 0 ... ingredients.size - 1
-      ingredient = CGI.escape(ingredients[i].title.downcase)
-      ingredients_url << ingredient << "%2c"
-    end
-
-    ingredients_url << CGI.escape(ingredients[ingredients.size - 1].title.downcase)
-
-    number = 5
-    ranking = 1
-    ignore_pantry = false
-
-    url =  "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?" + 
-           "number=#{number}" + 
-           "&ranking=#{ranking}" +
-           "&ignorePantry=#{ignore_pantry}" +
-           "&ingredients=#{ingredients_url}" 
-
-
     response = HTTParty.get(url, :headers => headers)
     recipes = JSON.parse(response.body)
-    filteredRecipes = json.select{ |recipe| recipe[:missedIngredientCount] == 0}
+    filteredRecipes = recipes.select{ |recipe| recipe[:missedIngredientCount] == 0}
+    render json: recipes
+  end
 
-    render json: filteredRecipes
-    
+  # GET /recipes/info
+  def info
+    headers = { 
+      "X-RapidAPI-Host" => "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+      "X-RapidAPI-Key" => "e20da5f30amsh2c5e4accedfa8e3p16b473jsnf1abb9f13716",
+      "Content-Type" => "application/x-www-form-urlencoded"
+    }
+    url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/#{params[:id]}/information"
+    response = HTTParty.get(url, :headers => headers)
+    render json: response.body
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'recipes', to: 'recipes#index'
+  get 'recipes/info', to: 'recipes#info'
   resources :ingredients
 	post 'auth/register', to: 'users#register'
   post 'auth/login', to: 'users#login'
